a17cde23be8dac4a583ef4a9027721eddc8af051
Anupamaraie <-->
2023-04-18 22:27:40 +0545

    Viewsets

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -1,7 +1,38 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-     path('books', views.books),
-     path('',views.home),
-]
+#urlpatterns = [
+     # path('books', views.books),
+     # path('',views.home),
+     # path('orders',views.Orders.listOrders)
+     # path('books/<int:pk>',views.BookView.as_view())
+# 	path('books', views.BookView.as_view(
+#           {
+#                'get': 'list',
+#                'post': 'create',
+#           })
+# 	),
+#      path('books/<int:pk>',views.BookView.as_view(
+#      {
+#                'get': 'retrieve',
+#                'put': 'update',
+#                'patch': 'partial_update',
+#                'delete': 'destroy',
+#       })
+# 	)
+# ]
+
+
+#Routing with SimpleRouter class in DRF
+# from rest_framework.routers import SimpleRouter
+
+# router = SimpleRouter(trailing_slash=False)
+# router.register('books',views.BookView, basename='books')
+# urlpatterns = router.urls
+
+#Routing with DefaultRouter class in DRF
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter(trailing_slash=False)
+router.register('books',views.BookView, basename='books')
+urlpatterns = router.urls
--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -6,24 +6,69 @@ from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
 
 # Create your views here.
-@csrf_exempt
-def books(request):
-    if request.method == 'GET':
-        books = Book.objects.all().values()
-        return JsonResponse({'books':list(books)})
-    elif request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        price = request.POST.get('price')
-        inventory = request.POST.get('inventory')
-        book = Book(title=title, author=author, price=price, inventory=inventory)
+# @csrf_exempt
+# def books(request):
+#     if request.method == 'GET':
+#         books = Book.objects.all().values()
+#         return JsonResponse({'books':list(books)})
+#     elif request.method == 'POST':
+#         title = request.POST.get('title')
+#         author = request.POST.get('author')
+#         price = request.POST.get('price')
+#         inventory = request.POST.get('inventory')
+#         book = Book(title=title, author=author, price=price, inventory=inventory)
 
-        try:
-            book.save()
-        except IntegrityError:
-            return JsonResponse({'error':'true','message':'required field missing'},status=400)
+#         try:
+#             book.save()
+#         except IntegrityError:
+#             return JsonResponse({'error':'true','message':'required field missing'},status=400)
 
-        return JsonResponse(model_to_dict(book), status=201)
+#         return JsonResponse(model_to_dict(book), status=201)
 
-def home(request):
-    return HttpResponse("Hi there.")
+# def home(request):
+#     return HttpResponse("Hi there.")
+
+#Routing to a class method
+
+from rest_framework.decorators import api_view, APIView
+
+from rest_framework.response import Response
+from rest_framework import status
+
+# class Orders():
+#     @staticmethod
+#     @api_view()
+#     def listOrders(request):
+#         return Response({'message':'list of orders'},200)
+
+
+#Routing class-based views
+
+# class BookView(APIView):
+
+#     def get(self, request, pk):
+#          return Response({"message":"single book with id " + str(pk)}, status=status.HTTP_200_OK)
+
+#     def put(self, request, pk):
+#          return Response({"title":request.data.get('title')}, status =status.HTTP_200_OK)
+
+
+#Routing classes that extend viewsets
+from rest_framework import viewsets
+
+class BookView(viewsets.ViewSet):
+	def list(self, request):
+        	return Response({"message":"All books"}, status.HTTP_200_OK)
+	def create(self, request):
+        	return Response({"message":"Creating a book"}, status.HTTP_201_CREATED)
+	def update(self, request, pk=None):
+        	return Response({"message":"Updating a book"}, status.HTTP_200_OK)
+	def retrieve(self, request, pk=None):
+        	return Response({"message":"Displaying a book"}, status.HTTP_200_OK)
+	def partial_update(self, request, pk=None):
+            return Response({"message":"Partially updating a book"}, status.HTTP_200_OK)
+	def destroy(self, request, pk=None):
+        	return Response({"message":"Deleting a book"}, status.HTTP_200_OK)
+
+
+
